69ece0d7f1ae98ce72a976635b1d4b096f4e8567
KevinHernandezRamos <kevinramos10055@gmail.com>
2025-07-05 22:21:35 -0600

    1ER COMMIT

---
+++ P1_2A_CLA/P2/4_proyecto_agenda_v1/main.py
@@ -0,0 +1,35 @@
+import agenda
+
+def main():
+    agenda_contactos = {} 
+
+    opcion=True
+    while opcion:
+     agenda.borrarPantalla()
+     opcion=agenda.menu_principal()
+     match opcion:
+        case "1":  
+            agenda.agregar_contacto(agenda_contactos)
+            agenda.esperarTecla()
+        case "2":
+            agenda.mostrar_contacto(agenda_contactos)
+            agenda.esperarTecla() 
+        case "3":
+            agenda.buscar_contacto(agenda_contactos)
+            agenda.esperarTecla()   
+        case "4":  
+            agenda.modificar_contacto(agenda_contactos)
+            agenda.esperarTecla()
+        case "5":
+            agenda.eliminar_contacto(agenda_contactos)
+            agenda.esperarTecla() 
+        case "6":
+            opcion=False    
+            agenda.borrarPantalla()
+            print(" Terminaste la ejecucion del SW ❗❗")
+        case _:
+            opcion=True 
+            print("❌ Opción invalida vuelva a intentarlo ❌") 
+            agenda.esperarTecla()
+if __name__ == "__main__":
+    main()
